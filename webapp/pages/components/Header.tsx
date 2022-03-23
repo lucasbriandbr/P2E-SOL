c611f65472d694dcf0bfc11b7cc26e96591075aa
lucas <Lucasbriandbr@gmail.com>
2022-03-23 12:55:26 +0100

    Header

--- webapp/pages/components/Header.tsx
+++ webapp/pages/components/Header.tsx
@@ -1,16 +1,28 @@
 // components/Header.js
+import styles from '../../styles/Header.module.css'
 
-const headerStyle = {
-    backgroundColor: "blue",
-    color: "white",
-    width: "100%",
-    height: "50px"
-  };
-  
-  const Header = () => (
-    <div className="Header" style={headerStyle}>
-      HEADER
+const Header = () => (
+  <div className={styles.Header}>
+    
+    <div className={styles.firstSection}>
+
+      <p>firstSection</p>
+
     </div>
-  );
+
+    <div className={styles.centerSection}>
+
+      <p>centerSection</p>
+
+    </div>
+
+    <div className={styles.lastSection}>
+
+      <p>lastSection</p>
+
+    </div>
+
+  </div>
+);
   
-  export default Header;
+export default Header;
