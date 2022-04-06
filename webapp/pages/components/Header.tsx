691d374349bbb4af844d5dd6ec2610785debbe71
lucas <Lucasbriandbr@gmail.com>
2022-04-06 12:41:46 +0200

    Bonfida domain

--- webapp/pages/components/Header.tsx
+++ webapp/pages/components/Header.tsx
@@ -4,19 +4,30 @@ import styles from '../../styles/Header.module.css'
 import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react';
 
+//librairie solana-web3.js
+
+import * as solanaWeb3 from '@solana/web3.js';
+import { clusterApiUrl, Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
+
+//Librairie pour la détection des NFT présents dans le wallet.
+
+import { getParsedNftAccountsByOwner, isValidSolanaAddress, createConnectionConfig } from "@nfteyez/sol-rayz";
+
+//librairie pour s'occupper des noms de domaines en .sol
+
+import { FavouriteDomain, NAME_OFFERS_ID } from "@bonfida/name-offers";
+import { performReverseLookup } from "@bonfida/spl-name-service";
+import { PublicKey } from "@solana/web3.js";
+
 function Header() {
 
   const [walletAdress, setWalletAdress] = useState('');
+  const [bonfidaDomain, setBonfidaDomain] = useState('');
   const [connected, setConnected] = useState(false);
   const [myProvider, setProvider] = useState();
+  const [balance, setBalance] = useState('');
 
   useEffect(() => {
-    
-    //Au lancement de la page
-    
-    getProviderHook();
-
-    //appel de la fonction de connexoin si déjà connecté
 
   });
 
@@ -24,46 +35,6 @@ function Header() {
   
   function changeUrl(url) {
     router.push(url, undefined, { shallow: true })
-  }
-
-  async function getProviderHook() {
-
-    if ("solana" in window) {
-
-      //si "solana" est détecté dans l'objet window : 
-
-      const provider = window.solana;
-
-      //on instaure cet objet en tant que provider
-
-      if (provider.isPhantom) {
-
-        //Si le provider est bien Phantom Wallet
-
-        const resp = await provider.connect({ onlyIfTrusted: true });
-
-        //On connecte le provider dans le cas ou l'on apparaît comme site de confiance
-
-        if (provider.isConnected == true) {
-
-          //si le provider est connecté
-          
-          setConnected(true);
-
-          //on définit un état connecté
-
-          setWalletAdress(resp.publicKey.toString());
-
-          //on passe l'adresse du wallet dans la fonction
-
-        } else {
-
-        }
-      }
-    } else {
-      // window.open("https://phantom.app/", "_blank")
-    }
-
   }
 
   async function getProvider() {
@@ -75,6 +46,8 @@ function Header() {
       //si "solana" est détecté dans l'objet window : 
 
       const provider = window.solana;
+      setProvider(provider);
+      // console.log(myProvider);
 
       //on instaure cet objet en tant que provider
 
@@ -102,6 +75,26 @@ function Header() {
 
             //on passe l'adresse du wallet dans la fonction
 
+            let connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("mainnet-beta"));
+
+            // let connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl("testnet"));
+
+            let accountTokens = await connection.getBalance(window.solana.publicKey);
+
+            setBalance((accountTokens/solanaWeb3.LAMPORTS_PER_SOL).toString());
+
+            //récupérer toutes le données du NFT
+
+            let solanadomain = await findFavoriteDomainName(resp.publicKey, connection);
+
+            if (typeof(solanadomain) === 'string') {
+
+              setBonfidaDomain(solanadomain);
+
+            }
+
+            // getAllNftData();
+
           } else {
 
           }
@@ -110,15 +103,29 @@ function Header() {
 
           //On attrappe les erreurs éventuelles
 
-          console.log(err);
+          if (err.code === 4001) {
 
-          //On affiche l'erreur attrappée
+          }
 
         }
       }
     } else {
-      // window.open("https://phantom.app/", "_blank");
+
+      window.open("https://phantom.app/", "_blank");
+
+      //On renvoie l'user vers la page de phantom Wallet
+
     }
+  }
+
+  //Se déconnecter grâce à une fonction
+
+  async function phantomDisconnect() {
+
+    myProvider.disconnect();
+
+    setConnected(false);
+
   }
 
   //Gérer les états ouvert et fermé du menu Hamburger Responsive
@@ -138,6 +145,50 @@ function Header() {
     setWalletOpen(!isWalletOpen);
     setHamburgerOpen(false);
   }
+
+  //fonction de récupération des NFT
+
+  
+  const getAllNftData = async () => {
+    
+    try {
+
+        const connect = createConnectionConfig(clusterApiUrl("mainnet-beta"));
+        const provider = window.solana;
+        let ownerToken = provider.publicKey;
+        const result = isValidSolanaAddress(ownerToken);
+
+        const nfts = await getParsedNftAccountsByOwner({
+
+          publicAddress: ownerToken,
+          connection: connect,
+          sanitize: true,
+
+        });
+
+    } catch (error) {
+
+      // console.log(error);
+
+    }
+
+  }
+
+  //récupérer les éventuels noms de domaines détenus par le wallet
+
+  const findFavoriteDomainName = async (owner: PublicKey, connection: solanaWeb3.Connection) => {
+    try {
+      const [favKey] = await FavouriteDomain.getKey(NAME_OFFERS_ID,new PublicKey(owner));
+  
+      const favourite = await FavouriteDomain.retrieve(connection, favKey);
+  
+      const reverse = await performReverseLookup(connection,favourite.nameAccount);
+  
+      return reverse;
+    } catch (err) {
+      // console.log(err);
+    }
+  };
 
   //Retour de la barre de Header
 
@@ -164,8 +215,16 @@ function Header() {
           {connected === true && walletAdress !== '' ?
 
           <div className={styles.hamburger} onClick={setWallet}>
+            
+            {connected === true && bonfidaDomain !== '' ?
+            
+            <p>{bonfidaDomain.replace(bonfidaDomain[0], bonfidaDomain[0].toUpperCase())}</p>
+
+            :
 
             <p>{walletAdress.substring(0,4)}...{walletAdress.substring(walletAdress.length-4,walletAdress.length)}</p>
+
+            }
 
           </div>
 
@@ -197,11 +256,11 @@ function Header() {
     
         <div className={`${styles.centerSectionWallet} ${isWalletOpen ? `${styles.hamburgermenuIsOpen}` : ``}`}>
     
-          <button className={styles.menuLink} disabled>Mainnet</button>
+          <button className={styles.menuLink} disabled><span className={styles.mainnetActive}></span> Mainnet</button>
     
-          <button className={styles.menuLink} disabled>Balance</button>
+          <button className={styles.menuLink} disabled>{balance.substring(0,balance.indexOf('.')+3)} SOL</button>
     
-          <button className={styles.menuLink} disabled>Disconnect</button>
+          <button className={styles.menuLink} onClick={phantomDisconnect}>Disconnect</button>
     
         </div>
         
