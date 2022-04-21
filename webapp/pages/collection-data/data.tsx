fdd419ec268be2fe2db80da4e10b997227ebdf7b
lucas <Lucasbriandbr@gmail.com>
2022-04-21 15:14:18 +0200

    Delete data.tsx

--- webapp/pages/collection-data/data.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-
-export default function GetMagicEdenData() {
-    const [deladata, setDeladata] = useState()
-    fetch('https://api-mainnet.magiceden.dev/v2/collections/runcible/stats')
-      .then(response => response.json())
-      .then(data => setDeladata(data))
-    return(deladata)
-}
