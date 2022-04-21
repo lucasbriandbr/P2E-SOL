88c254c8d1b3e3fb18172e5d794a4701326850d8
lucas <Lucasbriandbr@gmail.com>
2022-04-21 15:08:10 +0200

    ptn

--- webapp/pages/admin_pages/sales.tsx
+++ webapp/pages/admin_pages/sales.tsx
@@ -1,6 +1,22 @@
 import styles from "../../styles/Admin.module.css"
+import { useEffect, useState } from "react"
 
 export default function Sales() {
+
+    const [deladata, setDeladata] = useState()
+
+    useEffect(() => {
+
+        function GetMagicEdenData() {
+        fetch('https://api-mainnet.magiceden.dev/v2/collections/runcible/stats')
+          .then(response => response.json())
+          .then(data => console.log(data.symbol, data.floorPrice, data.listedCount, data.volumeAll))
+        }
+
+        GetMagicEdenData()
+
+    }, [])
+
     return(
         <div className={styles.whitelistGestion}>
             
